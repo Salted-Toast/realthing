c8f75be882235add54cd54ef90b7043d714f630c
Salted-Toast <104316350+morrison-page@users.noreply.github.com>
2023-03-07 21:50:39 +0000

    ...

--- contact.php
+++ contact.php
@@ -12,16 +12,19 @@
 
     <!-- Content -->
     <div class="container fullHeight d-flex align-items-center justify-content-center">
-        <div class="row d-flex justify-content-center text-center">
+        <div class="col d-flex justify-content-center text-center">
             <div class="col-md-6 col-sm-12 d-flex align-items-center flex-column pt-5">
                 <p>Email: healthadv@healthadv</p>
                 <p>Tel: +44 07455 569875</p>
                 <p>Address: Knightstone Campus, Knightstone Rd, Weston-super-Mare</p>
                 <p>Postcode: BS23 2AL</p>
             </div>
-            <div class="col-md-6 col-sm-12 d-flex align-items-center">
-                <iframe width="450" height="300" style="border:0;" loading="lazy" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d188997.08177018774!2d-0.18360680569105697!3d51.508924998036534!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47d8a00baf21de75%3A0x52963a5addd52a99!2sLondon!5e0!3m2!1sen!2suk!4v1648731298364!5m2!1sen!2suk"></iframe>
+            <div class="col">
+                <div class="mapouter">
+                    <div class="gmap_canvas"><iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=2880%20Broadway,%20New%20York&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://embedgooglemap.net/124/"></a><br><style>.mapouter{position:relative;text-align:right;height:500px;width:600px;}</style><a href="https://www.embedgooglemap.net"></a><style>.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:600px;}</style></div>
+                </div>
             </div>
+            
         </div>
     </div>
 
