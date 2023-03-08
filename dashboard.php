11808c24028397c2c6f6cd6f1002d51f9d908280
Salted-Toast <104316350+morrison-page@users.noreply.github.com>
2023-03-08 00:57:47 +0000

    ...

--- dashboard.php
+++ dashboard.php
@@ -3,41 +3,43 @@
 <head>
     <!-- Head -->
    <?php require 'includes/head.php'; ?>
-   <!-- Login Check -->
-   <?php loginCheck(); ?>
 </head>
 <body>
     <!-- Navbar -->
     <?php require 'includes/nav.php'; ?>
-    <!-- Connection -->
-    <?php require 'scripts/connect.php'; ?>
 
     <!-- Content -->
     <div class="container-fluid">
-            <div class="row">
-                <div class="col">
+        <div class="row">
+            <div class="col">
+                <div class="row">
                     <div class="card" style="margin-top: 12px;">
+                        <?php echo '<h1 style="text-align:center;">The current location selected is: ' . 'London' . '</h1>'?>
                         <script src="javascript/userLocation.js"></script>
                         <div id="location"></div>
                     </div>
-                    <div class="card" style="margin-top: 12px;">
-                        <!-- Pollution Graph -->
-                        <?php require 'includes/charts/polutionData.php'; ?>
-                    </div>
                 </div>
-                <div class="col">
+                <div class="row">
                     <div class="card" style="margin-top: 12px;">
                         <!-- Forecast Graph -->
                         <?php require 'includes/charts/forecastData.php'; ?>    
                     </div>
                 </div>
             </div>
+            <div class="col">
+                <div class="card" style="margin-top: 12px;">
+                    <!-- Pollution Graph -->
+                    <?php require 'includes/charts/polutionData.php'; ?>
+                </div>
+            </div>
         </div>
-    </div>
-
+        
     <!-- Footer -->
     <?php require 'includes/footer.php'; ?>
     <!-- JS Bootstrap -->
     <?php require 'includes/bootstrapjs.php'; ?>
 </body>
 </html>
+
+
+
