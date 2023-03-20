3d5ae6662b476f7a4bd111156f5f1b90d784cda2
Salted-Toast <104316350+morrison-page@users.noreply.github.com>
2023-03-20 00:35:00 +0000

    ...

--- test.php
+++ test.php
@@ -1,17 +1,37 @@
-<?php
-    // Store all relevent info to be used in requests
-    $apiKey = '9ec72fafc67f2ebbe14095e1c5426123';
-    $units = 'metric'; // Celsius
-    $lat = '51.5072178'; // latitude Coord
-    $lon = '-0.1275862'; // Longitude Coord
-    $days = '7'; // Days of data
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php require 'scripts/utils.php'; ?>
+</head>
+<body>
+    <?php
+        // Grab Values
+        $location = 'London';
 
-    // Craft the request
-    $apiUrl = "https://api.openweathermap.org/data/2.5/forecast/daily?lat={$lat}&lon={$lon}&cnt={$days}&units={$units}&appid={$apiKey}"; 
+        // Get user Coords
+        $coords = userCoords($location);
+        if ($coords != null) {
+            $lat = $coords[0];
+            $lon = $coords[1];
+        };
+    ?>
+    <?php
+        if (!isset($lat, $lon)) {
+            exit();
+        };
 
-    $requestFile = file_get_contents($apiUrl);
+        // Create API Requests
+        $apiKey = '9ec72fafc67f2ebbe14095e1c5426123';
 
-    $weatherData = json_decode($requestFile, true);
-
-    echo $weatherData;
-?>
+        // API request for polutants
+        $url = "https://api.openweathermap.org/data/2.5/forecast?lat={$lat}&lon={$lon}&unit=metric&appid={$apiKey}";
+        // Make Request and Decode into a JSON (True means assoc)
+        $polutionData = json_decode(file_get_contents($url),true);
+        echo (json_encode($polutionData));
+        // Extact Values From JSON
+    ?>
+</body>
+</html>
